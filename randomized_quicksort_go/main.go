859c8e944e50eaf690932e437d6c4acba070a6a6
V1eiraz <davienzo984@gmail.com>
2026-05-06 09:05:42 -0300

    Add randomized quicksort go and rename some folders

---
+++ randomized_quicksort_go/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func clearOutput() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func quicksort(arr []int, left int, right int) {
+	if left < right {
+		pivo := partition(arr, left, right)
+		quicksort(arr, left, pivo-1)
+		quicksort(arr, pivo+1, right)
+	}
+}
+
+func partition(arr []int, left int, right int) int {
+	randPos := left + rand.Intn(right-left+1)
+	arr[randPos], arr[right] = arr[right], arr[randPos]
+
+	pivo := arr[right]
+	i := left - 1
+	for j := left; j < right; j++ {
+		if arr[j] <= pivo {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[right] = arr[right], arr[i+1]
+	return i + 1
+}
+
+func readArchive() ([]int, int) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		fmt.Println("Erro ao obter diretório atual!")
+		return []int{}, 0
+	}
+	dir := filepath.Dir(filename)
+
+	path := filepath.Join(dir, "config", "input.dat")
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Arquivo não encontrado em: %s\n", path)
+		return []int{}, 0
+	}
+
+	lines := strings.Split(strings.ReplaceAll(string(content), "\r\n", "\n"), "\n")
+	if len(lines) < 2 {
+		return []int{}, 0
+	}
+
+	n, _ := strconv.Atoi(strings.TrimSpace(lines[0]))
+	numsStr := strings.Fields(lines[1])
+
+	nums := make([]int, 0, len(numsStr))
+	for _, s := range numsStr {
+		val, _ := strconv.Atoi(s)
+		nums = append(nums, val)
+	}
+
+	return nums, n
+}
+
+func writeArchive(arr []int) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		fmt.Println("Erro ao obter diretório atual!")
+		return
+	}
+	dir := filepath.Dir(filename)
+
+	path := filepath.Join(dir, "outputs", "output.dat")
+
+	os.MkdirAll(filepath.Dir(path), os.ModePerm)
+
+	file, err := os.Create(path)
+	if err != nil {
+		fmt.Println("Erro ao escrever arquivo!")
+		return
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, num := range arr {
+		writer.WriteString(strconv.Itoa(num))
+		writer.WriteString("\n")
+	}
+	writer.Flush()
+}
+
+func main() {
+	clearOutput()
+
+	var temposExecucao []float64
+	var finalNums []int
+
+	for i := 0; i < 50; i++ {
+		rand.Seed(67)
+		nums, n := readArchive()
+
+		if n > 0 {
+			start := time.Now()
+			quicksort(nums, 0, n-1)
+			elapsed := time.Since(start).Seconds()
+
+			temposExecucao = append(temposExecucao, elapsed)
+			finalNums = nums
+		}
+	}
+
+	fmt.Printf("Tempo de execução: %v\n\n", temposExecucao)
+
+	if len(temposExecucao) > 0 {
+		var soma float64
+		for _, t := range temposExecucao {
+			soma += t
+		}
+		mediaTempo := soma / float64(len(temposExecucao))
+		fmt.Printf("Média de tempo de execução do Randomized QuickSort (50 iterações): %.6f segundos\n", mediaTempo)
+
+		writeArchive(finalNums)
+	} else {
+		fmt.Println("Nenhum dado processado.")
+	}
+}
